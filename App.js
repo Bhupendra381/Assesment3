f7e9e8933b11215db610c46babf2c870e9f33070
Bhupendra381 <68839738+Bhupendra381@users.noreply.github.com>
2020-08-04 13:59:13 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import AppRouter from '../Assesment3/src/navigation/Nav'
+
+class App extends Component{
+	render(){
+		return(
+			<AppRouter/>
+		)
+	}
+}
+export default App;
